f8e8a045672131be826f86a28d422fec6eecc804
LitvinovaV <vaivanovava@gmail.com>
2023-11-23 17:22:09 +0300

    init commit

--- service/src/main/java/ru/diasoft/micro/repository/SmsVerificationRepository.java
+++ service/src/main/java/ru/diasoft/micro/repository/SmsVerificationRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import ru.diasoft.micro.domain.SmsVerification;
 
+import java.util.Optional;
+
 @Repository
 public interface SmsVerificationRepository extends JpaRepository<SmsVerification, Long> {
+    Optional<SmsVerification> findByCodeAndProcessGUID();
 }
---
+++ service/src/main/java/ru/diasoft/micro/service/SmsVerificationPrimaryService.java
@@ -0,0 +1,40 @@
+package ru.diasoft.micro.service;
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+import ru.diasoft.micro.controller.dto.SmsVerificationCheckRequest;
+import ru.diasoft.micro.controller.dto.SmsVerificationCheckResponse;
+import ru.diasoft.micro.controller.dto.SmsVerificationPostRequest;
+import ru.diasoft.micro.controller.dto.SmsVerificationPostResponse;
+import ru.diasoft.micro.domain.SmsVerification;
+import ru.diasoft.micro.repository.SmsVerificationRepository;
+
+import java.util.Random;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+@Service
+@Primary
+public class SmsVerificationPrimaryService implements SmsVerificationService{
+    private final SmsVerificationRepository repository;
+    @Override
+    public SmsVerificationCheckResponse dsSmsVerificationCheck(SmsVerificationCheckRequest smsVerificationCheckRequest) {
+        return null;
+    }
+
+    @Override
+    public SmsVerificationPostResponse dsSmsVerificationCreate(SmsVerificationPostRequest smsVerificationPostRequest) {
+        String GUID = UUID.randomUUID().toString();
+        String code = String.format("%04d", new Random().nextInt(10000));
+        SmsVerification smsVerification = SmsVerification.builder()
+                .phoneNumber(smsVerificationPostRequest.getPhoneNumber())
+                .processGuid(GUID)
+                .secretCode(code)
+                .status("WAITING")
+                .build();
+        SmsVerification saved = repository.save(smsVerification);
+        return new SmsVerificationPostResponse(saved.getProcessGuid());
+    }
+}
